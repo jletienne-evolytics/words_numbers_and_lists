d0f58bac4952a081b9f604e40b6dd912bb4004e0
Jean-Luc <53945245+jletienne-evolytics@users.noreply.github.com>
2024-08-06 21:52:26 -0500

    Update words_5c.py

--- 1_words/words_5c.py
+++ 1_words/words_5c.py
@@ -10,7 +10,7 @@ num_2 = 20
 num_3 = 100
 
 #Written as an f-string
-print(f'We made a calculator, throw away your TI-83, the answer is {num_1 + num_2 - num_3}')
+print(f'We made a calculator, the answer is {num_1 + num_2 - num_3}')
 
 
 # we can do calculations in our f string. We're combing strings and integers.
