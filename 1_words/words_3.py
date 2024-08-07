36569e757d61b017f78c5603c4ba1014bb47618b
Jean-Luc <53945245+jletienne-evolytics@users.noreply.github.com>
2024-08-06 21:51:36 -0500

    Update words_3.py

--- 1_words/words_3.py
+++ 1_words/words_3.py
@@ -4,8 +4,8 @@ print()
 #this is a variable
 name = 'Felicia'
 
-last_name = 'Chu'
+last_name = 'Data Science'
 
 #print an f-string, more info in the link
 #https://www.geeksforgeeks.org/formatted-string-literals-f-strings-python/
-print(f'My name is {name}, I cheat at fantasy football again. I repeat, my name is {name} {last_name}')
+print(f'My name is {name}, feel free to ask me about SQL, Python, or Fantasy Football Tips. I repeat, my name is {name} {last_name}')
